924e1df6fae5597c19d8b04efb5c64c53e4bc66a
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2025-01-02 15:13:08 +0530

    navbar done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,16 +4,41 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <div className="flex flex-row justify-between items-center p-4 bg-white border-2 h-20 text-gray-800">
-        <div className="logo  border-2 ">
-            <img src="/logo.png" alt="logo" className="w-[10rem]"  />
+      <div className="flex flex-row justify-between items-center p-4 px-16 bg-white border-2 h-20 text-gray-800">
+        <div className="logo  ">
+          <Link to="/">
+            <img src="/logo.png" alt="logo" className="w-[10rem]" />
+          </Link>
         </div>
-        <div className="navigation border-2 ">
-             <Link to="/" >Home</Link>
-             <Link to="/" >Order</Link>
-             <Link to="/" >Documents</Link>
+        <div className="navigation  flex font-medium gap-6 items-center">
+          <Link
+            to="/"
+            className=" active:bg-sky-300 rounded text-sky-600 px-2 py-1 "
+          >
+            Home
+          </Link>
+          <Link to="/order" className=" hover:text-sky-600 transition-all">
+            Order
+          </Link>
+          <Link to="/documents" className=" hover:text-sky-600 transition-all">
+            Documents
+          </Link>
         </div>
-        <div className="profile border-2 "></div>
+        <div className="profile flex gap-4 items-center">
+          <div className="text-right">
+            <Link to="/profile">
+              <p className="font-bold">John Doe</p>
+            </Link>
+            <p className=" text-sm text-blue-700 font-medium">Admin</p>
+          </div>
+          <div className="rounded-full ">
+            <img
+              src="/profile.png"
+              alt="profile"
+              className="w-10 h-10 rounded-full"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
