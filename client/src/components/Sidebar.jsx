19841ebc9dfb913fcfea09dbb06990d5b5c24188
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2025-01-07 14:43:28 +0530

    Thats enough

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,36 +5,10 @@ import { FaCheckCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
-// const obj = {
-//   pending: {
-//     dot_div: <FaRegCircle className="text-3xl text-sky-500 bg-gray-200 rounded-full " />,
-//     btn_div: <div>
-//     <button className="btn border-2 border-slate-200 text-slate-600 text-xs rounded-full py-1 px-4 text-center">
-//       Pending
-//     </button>
-//   </div>,
-//   },
-//   current: {
-//     dot_div:<FaRegDotCircle className="text-3xl text-sky-500 bg-gray-200 rounded-full " />,
-//     btn_div: <div>
-//     <button className="btn  bg-blue-200 text-slate-800 text-xs flex rounded-full py-1 px-4 text-center">
-//       In Progress
-//     </button>
-//   </div>,
-//   },
-//   completed: {
-//     dot_div:<FaRegDotCircle className="text-3xl text-sky-500 bg-gray-200 rounded-full " />,
-//     btn_div: <div>
-//     <button className="btn border-2 b-green-200 text-slate-600 text-xs rounded-full py-1 px-4 text-center">
-//       Completed
-//     </button>
-//   </div>,
-//   },
-// };
+
 
 export default function Sidebar() {
   const location = useLocation();
-  // location.pathname === "/visa_type"
   return (
     <>
       <div className="left-side h-[80vh] flex flex-col gap-8 w-[25%]  bg-white rounded-md p-8 ">
@@ -73,7 +47,7 @@ export default function Sidebar() {
           </div>
           <div className="right flex flex-col gap-8">
             <div className="flex flex-col gap-2">
-              <Link to={"/"} className="text-xl text-slate-700 font-bold">
+              <Link to={"/"} className="text-xl text-slate-700 font-bold hover:text-sky-700">
                 Add Itinerary
               </Link>
               <div>
@@ -91,7 +65,7 @@ export default function Sidebar() {
             <div className="flex flex-col gap-2">
               <Link
                 to={"/visa_type"}
-                className="text-xl text-slate-700 font-bold"
+                className="text-xl text-slate-700 font-bold hover:text-sky-700 "
               >
                 Choose Visa Type
               </Link>
@@ -110,7 +84,7 @@ export default function Sidebar() {
             <div className="flex flex-col gap-2">
               <Link
                 to={"/document_require"}
-                className="text-xl text-slate-700 font-bold"
+                className="text-xl text-slate-700 font-bold hover:text-sky-700"
               >
                 Documents Require
               </Link>
